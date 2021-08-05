3450c7b7729ad1b0eca8c0e6ef46d66758fbfc63
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-05 14:32:09 -0500

    Creating custom type for the id attr to ensure correct values only

--- app/src/main/java/com/raywenderlich/android/petsave/common/domain/model/animal/details/AnimalWithDetails.kt
+++ app/src/main/java/com/raywenderlich/android/petsave/common/domain/model/animal/details/AnimalWithDetails.kt
@@ -1,11 +1,12 @@
 package com.raywenderlich.android.petsave.common.domain.model.animal.details
 
 import com.raywenderlich.android.petsave.common.domain.model.animal.AdoptionStatus
+import com.raywenderlich.android.petsave.common.domain.model.animal.Id
 import com.raywenderlich.android.petsave.common.domain.model.animal.Media
 import org.threeten.bp.LocalDateTime
 
 data class AnimalWithDetails(
-    val id: Long,
+    val id: Id,
     val name: String,
     val type: String,
     val details: Details,
