3c8095cbf7bae997dd48740b26c0b7dde9fd2f0a
Daniel Flores <daniel.flores.fullstack@gmail.com>
2021-08-05 14:32:09 -0500

    Creating custom type for the id attr to ensure correct values only

--- app/src/main/java/com/raywenderlich/android/petsave/common/domain/model/animal/Animal.kt
+++ app/src/main/java/com/raywenderlich/android/petsave/common/domain/model/animal/Animal.kt
@@ -1,9 +1,10 @@
 package com.raywenderlich.android.petsave.common.domain.model.animal
 
 import org.threeten.bp.LocalDateTime
+import java.lang.Exception
 
 data class Animal(
-    val id: Long,
+    val id: Id,
     val name: String,
     val type: String,
     val media: Media,
@@ -13,6 +14,29 @@ data class Animal(
 )
 
 @JvmInline
-value class Id(
-    val value: Long
-)
+value class Id(private val value: Long) {
+    init {
+        validate(value)
+    }
+    private fun validate(id: Long): Either<IdException, Boolean> {
+        return when {
+            id.hasInvalidValue() -> Either.Left(IdException.InvalidIdException(id))
+            id.exceedsLength() -> Either.Left(IdException.InvalidIdLengthException(id))
+            else -> Either.Right(true)
+        }
+    }
+}
+
+private fun Long.hasInvalidValue() =
+    this == 0L || this == -1L
+private fun Long.exceedsLength() =
+    this > Long.MAX_VALUE - 1
+
+sealed class Either<out A, out B> {
+    class Left<A>(val value: A): Either<A, Nothing>()
+    class Right<B>(val value: B): Either<Nothing, B>()
+}
+sealed class IdException(message: String): Exception(message) {
+    data class InvalidIdException(val id: Long): IdException("$id")
+    data class InvalidIdLengthException(val id: Long): IdException("$id")
+}
